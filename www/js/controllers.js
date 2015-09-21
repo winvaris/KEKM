e874ad7119249e828ee79b52b054cd27a1b9bfbb
tan_dooky <tan_dooky@windowslive.com>
2015-09-21 10:18:16 +0700

    tab login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $window) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -42,7 +42,10 @@ angular.module('starter.controllers', [])
 
   $scope.starTest = function() {
     console.log("TEST STAR");
-  }
+  };
+  $scope.toHome = function(){
+    $window.location.href = '/#/app/home';
+  };
 
 })
 
@@ -76,7 +79,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('LoginPageCtrl', function($scope, $stateParams) {
-  
+
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
